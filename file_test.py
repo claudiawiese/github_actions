3046ae6c9c4f9b04e0b450e3fe0ef2a9bae59c3a
claudiawiese <46198497+claudiawiese@users.noreply.github.com>
2024-02-28 16:48:19 +0100

    Update file_test.py

--- file_test.py
+++ file_test.py
@@ -1,22 +1,22 @@
 def test_calc_addition():
-  # Function test the output of 2+4 # noqa: E501
-  output = 2+4
-  assert output == 6
+    # Function test the output of 2+4 # noqa: E501
+    output = 2+4
+    assert output == 6
 
 
 def test_calc_substraction():
-  # Function test the output of 2-4 # noqa: E501
-  output = 2-4
-  assert output == -2
+    # Function test the output of 2-4 # noqa: E501
+    output = 2-4
+    assert output == -2
 
 
 def test_calc_multiply():
-  # Function test the output of 2*4 # noqa: E501
-  output = 2*4
-  assert output == 8
+    # Function test the output of 2*4 # noqa: E501
+    output = 2*4
+    assert output == 8
 
 
 def test_coucou():
-  # Function test if the output return 'hello' # noqa: E501
-  output = 'hello'
-  assert output == 'hello'
+    # Function test if the output return 'hello' # noqa: E501
+    output = 'hello'
+    assert output == 'hello'
