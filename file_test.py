f324c01088c9e8702035ddcca10925219d61ae4f
claudiawiese <46198497+claudiawiese@users.noreply.github.com>
2024-02-28 16:46:00 +0100

    Update file_test.py

--- file_test.py
+++ file_test.py
@@ -3,15 +3,18 @@ def test_calc_addition():
   output = 2+4
   assert output == 6
 
+
 def test_calc_substraction():
   # Function test the output of 2-4 # noqa: E501
   output = 2-4
   assert output == -2
 
+
 def test_calc_multiply():
   # Function test the output of 2*4 # noqa: E501
   output = 2*4
   assert output == 8
+
 
 def test_coucou():
   # Function test if the output return 'hello' # noqa: E501
